e5787906713223cd15f6855fe74e91f7567344d5
pythonzjw <782853016@qq.com>
2026-06-11 22:20:05 +0800

    fix: escape response public key string

--- android_app/app/src/main/java/com/zjw/visitorqrregister/AppConfig.java
+++ android_app/app/src/main/java/com/zjw/visitorqrregister/AppConfig.java
@@ -29,9 +29,12 @@ final class AppConfig {
 
     private static Map<String, String> buildResponsePublicKeys() {
         Map<String, String> keys = new HashMap<>();
-        keys.put("visitor_qr_register_v1", "-----BEGIN PUBLIC KEY-----
-MCowBQYDK2VwAyEAJsKZ/G/z1HuY1JahRjj+zYaxOmSXXMM49X2RCVLbU74=
------END PUBLIC KEY-----
+        keys.put("visitor_qr_register_v1",
+                "-----BEGIN PUBLIC KEY-----
+"
+                        + "MCowBQYDK2VwAyEAJsKZ/G/z1HuY1JahRjj+zYaxOmSXXMM49X2RCVLbU74=
+"
+                        + "-----END PUBLIC KEY-----
 ");
         return Collections.unmodifiableMap(keys);
     }
